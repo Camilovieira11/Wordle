26631544ed295827217efa14348e5a788fb1c888
Camilovieira11 <camilovieirafon@gmail.com>
2026-03-25 17:40:50 -0300

    Implemnetação de banco de dados

--- main.py
+++ main.py
@@ -1,43 +1,97 @@
-
 import os
 import requests
+import sqlite3
+from datetime import datetime
 
-response = requests.get("https://random-word-api.herokuapp.com/word?number=1&diff=1&lang=pt-br")
-palavra_secreta =  response.json()[0]
+# ---------------- BANCO ---------------- #
+
+def conectar():
+    return sqlite3.connect('dicionario.db')  # tirei o acento (evita erro)
+
+def criar_tabela():
+    conn = conectar()
+    cursor = conn.cursor()
+    
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS palavras (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            palavra TEXT,
+            data_criacao TEXT
+        )
+    ''')
+    
+    conn.commit()
+    conn.close()
+
+def salvar_palavras(palavra):
+    conn = conectar()
+    cursor = conn.cursor()
+    
+    data = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    cursor.execute('''
+        INSERT INTO palavras (palavra, data_criacao)
+        VALUES (?, ?)
+    ''', (palavra, data))
+    
+    conn.commit()
+    conn.close()
+
+# cria tabela ao iniciar
+criar_tabela()
+
+# ---------------- JOGO ---------------- #
+
+# pega palavra da API
+response = requests.get("https://random-word-api.herokuapp.com/word?number=1&lang=pt-br")
+
+if response.status_code == 200:
+    palavra_secreta = response.json()[0]
+else:
+    print("Erro ao buscar palavra da API.")
+    exit()
+
+
+# salva no banco
+salvar_palavras(palavra_secreta)
+
 letras_acertadas = ''
 numero_tentativas = 0
-limite_de_tentativas = len(palavra_secreta) 
+limite_de_tentativas = len(palavra_secreta)
+
 while True:
     if limite_de_tentativas <= 0:
-        print('acabaram as tentativas')
-        print('A palavra era ', palavra_secreta)
-        break     
-    letra_digitada = input('Digite uma letra: ')
+        print('Acabaram as tentativas!')
+        print('A palavra era:', palavra_secreta)
+        break
+
+    letra_digitada = input('Digite uma letra: ').lower()
+
+    if len(letra_digitada) != 1:
+        print('Digite apenas UMA letra.')
+        continue
+
     numero_tentativas += 1
     limite_de_tentativas -= 1
-    if len(letra_digitada) > 1:
-        print('Digite apenas uma letra.')
-        continue
 
     if letra_digitada in palavra_secreta:
         letras_acertadas += letra_digitada
 
     palavra_formada = ''
-    for letra_secreta in palavra_secreta:
-        if letra_secreta in letras_acertadas:
-            palavra_formada += letra_secreta
+    for letra in palavra_secreta:
+        if letra in letras_acertadas:
+            palavra_formada += letra
         else:
             palavra_formada += '*'
 
-    print('Palavra formada:', palavra_formada)
-    print("Tentativas restantes: ", limite_de_tentativas)
+    # limpar tela (compatível)
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    print('Palavra:', palavra_formada)
+    print('Tentativas restantes:', limite_de_tentativas)
+
     if palavra_formada == palavra_secreta:
-        os.system('clear')
-        print('VOCÊ GANHOU!! PARABÉNS!')
-        print('A palavra era', palavra_secreta)
+        print('\n🎉 VOCÊ GANHOU!')
+        print('Palavra:', palavra_secreta)
         print('Tentativas:', numero_tentativas)
-        print('Tentativas restantes:', limite_de_tentativas)
-        letras_acertadas = ''
-        numero_tentativas = 0
-        limite_de_tentativas = 0
-   
+        break
